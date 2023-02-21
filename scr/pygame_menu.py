8d9757915449a79057c3c51f2fe4199880c81c8a
Nemex81 <68394029+Nemex81@users.noreply.github.com>
2023-02-21 01:59:19 +0100

    Update pygame_menu.py

--- scr/pygame_menu.py
+++ scr/pygame_menu.py
@@ -1,33 +1,17 @@
 class PyMenu:
-    def __init__(self, options_dict, menu_title, escape_text):
-        self.options_dict = options_dict
-        self.menu_title = menu_title
-        self.escape_text = escape_text
-        self.id_focus = 0
-    
-    def display_menu(self):
-        vocalizza(self.menu_title)
-        for key in self.options_dict:
-            vocalizza(key)
-        vocalizza(self.escape_text)
-        self.id_focus = 0
-    
-    def navigate_menu(self, direction):
-        if direction == "up":
-            if self.id_focus > 0:
-                self.id_focus -= 1
-                vocalizza(list(self.options_dict.keys())[self.id_focus])
-            else:
-                self.id_focus = len(self.options_dict) - 1
-                vocalizza(list(self.options_dict.keys())[self.id_focus])
-        elif direction == "down":
-            if self.id_focus < len(self.options_dict) - 1:
-                self.id_focus += 1
-                vocalizza(list(self.options_dict.keys())[self.id_focus])
-            else:
-                self.id_focus = 0
-                vocalizza(list(self.options_dict.keys())[self.id_focus])
-    
-    def execute_selected_option(self):
-        selected_option = list(self.options_dict.values())[self.id_focus]
-        selected_option()
+    def __init__(self, items, callback, screen_reader):
+        self.items = items
+        self.selected_item = 0
+        self.callback = callback
+        self.screen_reader = screen_reader
+
+    def next_item(self):
+        self.selected_item = (self.selected_item + 1) % len(self.items)
+        self.screen_reader.vocalizza(self.items[self.selected_item])
+
+    def prev_item(self):
+        self.selected_item = (self.selected_item - 1) % len(self.items)
+        self.screen_reader.vocalizza(self.items[self.selected_item])
+
+    def execute(self):
+        self.callback(self.selected_item)
