11e3f2ae5dc8c0559a55e52ca4b379f001e49556
Nemex81 <68394029+Nemex81@users.noreply.github.com>
2023-02-23 00:42:48 +0100

    stabilizzato classe GamePlay

--- scr/game_play.py
+++ scr/game_play.py
@@ -28,19 +28,18 @@ class GamePlay:
 		self.selected_card = None  # carta selezionata dal cursore
 		self.selected_card_index = 0  # indice della carta selezionata nella pila di tableau
 		self.build_commands_list()
-		#self.new_game()
-		self.engine.crea_gioco()
+		self.new_game()
 
 	def create_tableau(self):
-		deck = self.engine.crea_mazzo() # crea un mazzo ordinato
+		deck = self.crea_mazzo() # crea un mazzo ordinato
 		random.shuffle(deck) # mescola il mazzo
 
 		# distribuisci le carte sul tableau
 		for i, row in enumerate(self.tableau):
 			for j in range(i+1):
 				card = deck.pop()
-				if j == i:
-					card.flip() # scopri l'ultima carta di ogni colonna
+				#if j == i:
+					#card.flip() # scopri l'ultima carta di ogni colonna
 				row.append(card)
 
 		self.waste_pile = [] # svuota la pila di scarti
@@ -84,53 +83,10 @@ class GamePlay:
 
 
 	def new_game(self):
-		self.create_tableau() # crea il tavolo di gioco
-		self.engine.distribuisci_carte()# distribuisci le carte sul tableau
-		self.draw_table() # disegna il tableau
-
-	def last_move_cursor_up(self):
-		if self.current_index > 0:
-			self.current_index -= 1
-		else:
-			self.current_index = len(self.menu_items) - 1
-
-	def last_move_cursor_dn(self):
-		if self.selected_card is not None:
-			# Check if selected card is last in tableau
-			last_card = self.tableau[self.selected_card[0]][-1]
-			if self.selected_card[1] == len(self.tableau[self.selected_card[0]])-1:
-				# Check if there is a card on the stock pile
-				if self.waste_pile:
-					self.selected_card = None
-					self.selected_pile = 'waste'
-				else:
-					return
-			else:
-				self.selected_card = (self.selected_card[0], self.selected_card[1]+1)
-			self.draw_table()
-
-	def last_select_card(self):
-		row, col = self.cursor_pos
-		card = self.tableau[row][col]
-
-		if card is not None:
-			self.selected_card = card
-
-	def last_move_card(self):
-		if self.selected_pile is None or self.destination_pile is None:
-			return
-
-		# Verifica se la mossa è valida
-		if self.game_engine.is_valid_card_move(self.selected_pile, self.destination_pile):
-			# Sposta la carta dalla pila di origine alla pila di destinazione
-			self.game_engine.move_card(self.selected_pile, self.destination_pile)
-
-			# Aggiorna lo stato del tavolo di gioco
-			self.update_game_state()
-
-			# Resetta le pile selezionate
-			self.selected_pile = None
-			self.destination_pile = None
+		#self.engine.create_tableau() # crea il tavolo di gioco
+		self.engine.crea_gioco()
+		#self.engine.distribuisci_carte()# distribuisci le carte sul tableau
+		#self.draw_table() # disegna il tableau
 
 	def is_valid_move(self, origin_pile, dest_pile):
 		# Check if origin and destination piles are valid
@@ -155,7 +111,7 @@ class GamePlay:
 			self.cursor_pos[0] -= 1
 
 	def move_cursor_right(self):
-		if self.cursor_pos[0] < len(self.tableau) - 1:
+		if self.cursor_pos[0] < len(self.engine.tableau) - 1:
 			self.cursor_pos[0] += 1
 
 	def move_cursor_up(self):
@@ -163,30 +119,43 @@ class GamePlay:
 			self.cursor_pos[1] -= 1
 
 	def move_cursor_down(self):
-		if self.cursor_pos[1] < len(self.tableau[self.cursor_pos[0]]) - 1:
+		if self.cursor_pos[1] < len(self.engine.tableau[self.cursor_pos[0]]) - 1:
 			self.cursor_pos[1] += 1
 
 	def select_card(self):
-		self.selected_card = self.tableau[self.cursor_pos[0]][self.cursor_pos[1]]
+		self.selected_card = self.engine.get_card(self.cursor_pos[0], self.cursor_pos[1])
 
 	def move_card(self):
-		self.engine.move_card()
-
-	def LAST_move_card(self):
-		if self.selected_card is None:
-			return
-
-		# Check if the move is valid
-		if self.engine.is_valid_card_move(self.selected_card, self.destination_pile):
-			# Move the card from the source pile to the destination pile
-			self.engine.move_card(self.selected_card, self.destination_pile)
-
-			# Update the game state
+		if self.selected_card is not None:
+			dest_row = self.cursor_pos[0]
+			dest_col = len(self.engine.tableau[dest_row])
+			self.engine.move_card(self.selected_card, dest_row, dest_col)
+			self.selected_card = None
 			self.update_game_state()
 
-			# Reset the selected and destination piles
-			self.selected_card = None
-			self.destination_pile = None
+	def update_game_state(self):
+		# Aggiorna lo stato del tavolo di gioco
+		self.engine.check_for_win()
+		self.engine.update_tableau()
+		self.engine.update_foundations()
+		self.engine.update_waste_pile()
+
+	def handle_keyboard_events(self, event):
+		if event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				self.running = False
+			elif event.key == K_LEFT:
+				self.move_cursor_left()
+			elif event.key == K_RIGHT:
+				self.move_cursor_right()
+			elif event.key == K_UP:
+				self.move_cursor_up()
+			elif event.key == K_DOWN:
+				self.move_cursor_down()
+			elif event.key == K_RETURN:
+				self.select_card()
+			elif event.key == K_SPACE:
+				self.move_card()
 
 	def quit_app(self):
 		self.screen_reader.vocalizza("chiusura in corso.  ")
@@ -194,28 +163,6 @@ class GamePlay:
 		result = self.dialog_box.create_question_box("Sei sicuro di voler uscire?")
 		if result:
 			self.is_running = False
-
-	def last_handle_keyboard_EVENTS(self, event):
-		#for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			self.running = False
-
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				self.running = False
-
-			elif event.key == pygame.K_LEFT:
-				self.selected_pile_index = (self.selected_pile_index - 1) % 7
-
-			elif event.key == pygame.K_RIGHT:
-				self.selected_pile_index = (self.selected_pile_index + 1) % 7
-
-			elif event.key == pygame.K_UP:
-				#self.gameplay.selected_card_index = (self.selected_card_index - 1) % len(self.game_engine.tableau[self.selected_pile_index])
-				if len(self.tableau[self.selected_pile_index]) > 0:
-					self.selected_card_index = (self.selected_card_index - 1) % len(self.tableau[self.selected_pile_index])
-				else:
-					self.selected_card_index = 0
 
 	def build_commands_list(self):
 		self.callback_dict = {
@@ -228,10 +175,12 @@ class GamePlay:
 			pygame.K_ESCAPE: self.quit_app,
 		}
 
-	def handle_keyboard_EVENTS(self, event):
+	def last_handle_keyboard_EVENTS(self, event):
 		if event.type == KEYDOWN:
 			if self.EVENTS_DN.get(event.key):
 				self.EVENTS_DN[event.key]()
+
+
 
 #@@@# start del modulo
 if __name__ == "__main__":
