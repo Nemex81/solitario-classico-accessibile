b7b47638cb29341720bf4f3ad4d8fde2c7e77aa5
Nemex81 <68394029+Nemex81@users.noreply.github.com>
2023-02-22 11:21:04 +0100

    Create gam_eplay.py
    creato il file per il tavolo di gioco.

---
+++ scr/gam_eplay.py
@@ -0,0 +1,26 @@
+"""
+	file game_play.py
+	percorso: https://github.com/Nemex81/solitario-classico-accessibile/blob/main/scr/game_play.py
+
+"""
+
+import sys, os, time, pygame
+from pygame.locals import *
+from my_lib.dialog_box import DialogBox
+from scr.pygame_menu import PyMenu
+
+# inizializzo pygame
+pygame.init()
+pygame.font.init()
+
+class GamePlay:
+	pass
+
+
+
+
+#@@@# start del modulo
+if __name__ == "__main__":
+	print("compilazione completata di %s" % __name__)
+else:
+	print("Carico: %s" % __name__)
